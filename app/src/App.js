50d476491844eabf574bf7941ec46ef012ca641f
Davide Cantelli <cant1989@gmail.com>
2018-11-22 23:50:18 +0100

    server and app v1 working

--- app/src/App.js
+++ app/src/App.js
@@ -4,16 +4,84 @@ const socket = openSocket('http://localhost:3001');
 
 class App extends Component {
 
-  componentDidMount(){
+  constructor(props) {
+    super(props)
+    this.state = {
+      nums: [],
+      gameState: "ready"
+    }
+  }
 
-      socket.on('round', ()=>console.log("round arrived from server"));
+  actions = [-1,0,1]
+  resetterEndStates = ["win", "lose", "user disconnected"]
+
+  componentDidMount(){
+    socket.once("busy", ()=>this.updateGameState({status:"busy"}))
+  }
+  
+  startGame = () => {
+    socket.on('play', this.onPlay);
+    socket.once('end', this.updateGameState)
+    socket.emit('start')
+    this.updateGameState({status:"waiting"})
+  }
+
+  updateGameState = ({ status })=>{
+    let newState = {
+      gameState: status
+    }
+    if(this.resetterEndStates.indexOf(status) > -1)
+      newState.nums = []
+
+    this.setState(newState)
+  }
+
+  onPlay = (num)=>{
+    this.setState({
+      nums: [...this.state.nums, num],
+      gameState: "playing"
+    }, ()=>{
+      console.log(this.state)
+    })
+  }
+
+  handlePlay = (action)=>{
+    this.setState({
+      gameState: "waiting"
+    }, () => {
+      socket.emit("played", action)
+    })    
   }
 
   render() {
+
+    const {gameState, nums} = this.state
+    console.log(this.state)
+
     return (
       <div>
         <header>Play with the chance</header>
         <main>
+
+          {gameState==="busy"
+            ? <div>Game is busy, someone else is playing. Please wait.</div>
+            : <div>
+                <button onClick={this.startGame}>Start</button>
+                {this.actions.map(action=>(
+                  <button 
+                    onClick={()=>this.handlePlay(action)}>
+                    {action}
+                  </button>)
+                )}
+              </div>
+          }
+
+          <ul>
+            {nums.map(num=>(
+              <li>{num}</li>
+              ))}
+          </ul>
+          
         </main>
       </div>
     );
--- server/index.js
+++ server/index.js
@@ -1,15 +1,94 @@
 const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const uniqid = require('uniqid');
 
-app.get('/', function(req, res){
-  res.send('hello!');
-});
+let game
+resetGame()
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
+server.listen(3001, () => console.log('listening on *:3001'));
 
-http.listen(3001, function(){
-  console.log('listening on *:3001');
+function genRandomNum() {
+  return Math.floor(Math.random() * 100)
+}
+
+function getNextNum(num) {
+  return Math.round(num / 3)
+}
+
+function isActionValid(action, num) {
+  const op = (num + action) / 3
+  return op === Math.round(op)
+}
+
+function resetGame(){
+  game = {
+    players: 0,
+    playing: false
+  }
+}
+
+function emitWin(socket) {
+  emitEnd(socket, "win")
+  emitEnd(socket.broadcast, "lose")
+}
+
+function emitLose(socket) {
+  emitEnd(socket, "lose")
+  emitEnd(socket.broadcast, "win")
+}
+
+function emitPlay(socket, num) {
+  socket.emit("play", num)
+}
+
+function emitEnd(socket, status){
+  socket.emit("end", {status})
+  resetGame()
+}
+
+function emitBusy(socket){
+  socket.emit("busy")
+}
+
+function startGame(socket){
+  console.log("ready to play")
+  game.playing = true
+  game.num = genRandomNum()
+  emitPlay(socket, game.num)
+}
+
+function onPlayed(socket, action){
+  if (!isActionValid(action, game.num)) {
+    emitLose(socket)
+    return
+  }
+
+  game.num = getNextNum(game.num)
+  game.num === 1 
+    ? emitWin(socket)
+    : emitPlay(socket.broadcast, game.num)
+}
+
+function userDisconnected(){
+  emitEnd(io, 'user disconnected')
+  resetGame()
+}
+
+function addPlayer(socket) {
+
+  if (game.playing) {
+    emitBusy(socket)
+    return
+  }
+
+  game.players++
+  socket.on("played", action=>onPlayed(socket, action))
+  game.players === 2 && startGame(socket)
+  socket.on('disconnect', userDisconnected)
+}
+
+io.on('connection', socket => {
+  console.log('new player');
+  socket.on('start', ()=>addPlayer(socket))
 });
