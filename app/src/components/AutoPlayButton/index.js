77911ce1b2b75511344dc64165a8ab2ec3738f3e
Davide Cantelli <davide.cantelli.externa@ynap.com>
2018-11-29 12:30:15 +0100

    style ++

--- app/src/components/AutoPlayButton/index.js
+++ app/src/components/AutoPlayButton/index.js
@@ -1,62 +1,78 @@
-import React, { Component } from 'react';
-import { gameStates } from '../../App';
+import React, { Component } from "react";
+import { gameStates } from "../../App";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  display: block;
+  margin: 5px auto;
+  border-radius: 100px;
+  height: 60px;
+  padding: 0 20px;
+  background: ${({ enabled }) => (enabled ? "#26a69a" : "#d05b5b")};
+  border: none;
+  color: #fff;
+  line-height: 60px;
+  cursor: pointer;
+  font-weight: 700;
+  outline: none;
+  box-shadow: 0px 3px 0 1px ${({ enabled }) => (enabled ? "#13837a" : "#9c3b3b")};
+  -webkit-tap-highlight-color:  rgba(255, 255, 255, 0); 
+`;
 
 class AutoPlayButton extends Component {
   state = {
     enabled: false
-  }
+  };
 
   getActionFromNum = num => {
-    const n = num / 3
-    return n === Math.round(n)
-      ? 0
-      : n < Math.round(n)
-        ? 1
-        : -1
-  }
+    const n = num / 3;
+    return n === Math.round(n) ? 0 : n < Math.round(n) ? 1 : -1;
+  };
 
   enableAutoPlay = () => {
-    const { num, autoPlay } = this.props
-    const action = this.getActionFromNum(num)
-    this.t = setTimeout(() => autoPlay(action), 3000)
-  }
+    const { num, autoPlay } = this.props;
+    const action = this.getActionFromNum(num);
+    this.t = setTimeout(() => autoPlay(action), 3000);
+  };
 
   disableAutoPlay = () => {
-    this.t && clearTimeout(this.t)
-  }
+    this.t && clearTimeout(this.t);
+  };
 
   manageAutoPlay = () => {
-    const { gameState } = this.props
+    const { gameState } = this.props;
 
     gameState === gameStates.playing
       ? this.enableAutoPlay()
-      : this.disableAutoPlay()
-  }
+      : this.disableAutoPlay();
+  };
 
   componentDidUpdate = () => {
-    this.state.enabled && this.manageAutoPlay()
-  }
+    this.state.enabled && this.manageAutoPlay();
+  };
 
   componentDidMount = () => {
-    this.state.enabled && this.manageAutoPlay()
-  }
+    this.state.enabled && this.manageAutoPlay();
+  };
 
   componentWillUnmount = () => {
-    this.state.enabled && this.disableAutoPlay()
-  }
+    this.state.enabled && this.disableAutoPlay();
+  };
 
   toggleAutoPlay = () => {
     this.setState({
       enabled: !this.state.enabled
-    })
-  }
+    });
+  };
 
   render() {
-    const { enabled } = this.state
+    const { enabled } = this.state;
     return (
-      <button onClick={() => this.toggleAutoPlay()}>{enabled ? "Autoplay enabled" : "Autoplay disabled"}</button>
-    )
+      <StyledButton enabled={enabled} onClick={() => this.toggleAutoPlay()}>
+        AI {enabled ? "ON" : "OFF"}
+      </StyledButton>
+    );
   }
 }
 
-export default AutoPlayButton
+export default AutoPlayButton;
